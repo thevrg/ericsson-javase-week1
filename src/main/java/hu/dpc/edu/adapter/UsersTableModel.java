d6aac80acbee91b277fe2d82975f8549ff6624f8
Peter Varga <peter.varga@dpc.hu>
2016-10-12 13:43:20 +0200

    UserFrame.java added

---
+++ src/main/java/hu/dpc/edu/adapter/UsersTableModel.java
@@ -0,0 +1,7 @@
+package hu.dpc.edu.adapter;
+
+/**
+ * Created by vrg on 12/10/16.
+ */
+public class UsersTableModel {
+}
---
+++ src/main/java/hu/dpc/edu/userGUI/TestFrame.java
@@ -0,0 +1,7 @@
+package hu.dpc.edu.userGUI;
+
+/**
+ * Created by vrg on 12/10/16.
+ */
+public class TestFrame {
+}
